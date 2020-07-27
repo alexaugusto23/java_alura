6b23bd7e39b2d34a3e1ac733e482738c09f27d0d
alexaugusto23 <contato.alexaugusto@hotmail.com>
2020-07-26 23:41:03 -0300

    novas aulas

---
+++ TestaPontoFlutuante.java
@@ -0,0 +1,21 @@
+
+public class TestaPontoFlutuante {
+	public static void main(String[] args) {
+		double salario;
+		salario = 1250.70;
+		System.out.println("meu salário é " + salario);
+		
+		double idade = 37;
+		
+		double divisao = 3.14 / 2;
+		System.out.println("Divisão:" + divisao);
+		
+		int outraDivisao = 5 / 2;
+		System.out.println("Outra divisão: " + outraDivisao);
+		
+		double novaTentativa = 5.0 / 2;
+		System.out.println("Nova tentativa: " + novaTentativa);
+		
+	}
+
+}
